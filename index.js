16d4579cbcf162336b43a4fdb141627227f3c67e
Rahul <venkatrahulxyz@gmail.com>
2025-01-27 11:00:37 +0530

    navbar completed

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ import env from "dotenv";
 
 env.config();
 const app=express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.get("/", (req, res)=>{
     res.render("index.ejs");
 });
+
+app.get("/problemslvfrm", (req, res)=>{
+    res.render("Problemslvfrm.ejs");
+})
 
 app.listen(process.env.PORT);
