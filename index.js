2e3c7d0e5aea445ee51e2f3004428cbbe5f5c4b3
Rahul <venkatrahulxyz@gmail.com>
2025-03-04 14:20:09 +0530

    github auth done

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import session from "express-session";
 import env from "dotenv";
 import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import { Strategy as GitHubStrategy } from "passport-github2";
 
 
 env.config();
@@ -30,30 +31,49 @@ passport.deserializeUser((user, done)=>{
 });
 
 //google strategy
-passport.use(new GoogleStrategy({
+passport.use("google", new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: "http://localhost:3000/auth/google/callback"
   },
   function(accessToken, refreshToken, profile, cb) {
-    console.log(profile);
+    // console.log(profile);
     return cb(null, profile);
   }
 ));
 
+//github strategy
+passport.use("github", new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/github/callback"
+}, (accessToken, refreshToken, profile, done)=>{
+    // console.log(profile);
+    return done(null, profile);
+}));
+
+
 app.get("/auth/google", passport.authenticate('google', { scope: ['profile'] }));
 
-app.get("/auth/google/callback", passport.authenticate('google', { failureRedirect: '/auth/google' }), (req, res)=>{
+app.get("/auth/google/callback", passport.authenticate('google', { failureRedirect: '/' }), (req, res)=>{
     res.redirect("/problemslvfrm");
 })
 
-app.get("/auth/google/logout", (req, res)=>{
+app.get("/logout", (req, res)=>{
     req.logout(function (err) {
         if (err) {
           return next(err);
         }
         res.redirect("/");
     });
+});
+
+app.get('/auth/github',passport.authenticate('github', { scope: [ 'user:email' ] }));
+
+app.get("/auth/github/callback", passport.authenticate("github", {
+    failureRedirect: "/",
+}), (req, res)=>{
+    res.redirect("/problemslvfrm");
 });
 
 
