5e940af90835c857f7910dc233c63eab1358d026
Rahul <venkatrahulxyz@gmail.com>
2025-03-11 22:30:50 +0530

    part2 ie scroller done

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,17 +1,19 @@
-const btns=document.getElementsByClassName("btn");
-const att_divs=document.getElementsByClassName("d_attendance");
+const btns = document.getElementsByClassName("btn");
+const att_divs = document.getElementsByClassName("d_attendance");
 
-let curratt=0
+let curratt = 0;
 
-const btnarray=Array.from(btns);
-btnarray.forEach((btn, index)=>{
-    btn.addEventListener("click", ()=>{
+const btnarray = Array.from(btns);
+btnarray.forEach((btn, index) => {
+    btn.addEventListener("click", () => {
         att_divs[curratt].classList.remove("active");
         att_divs[curratt].style.display = "none";
         att_divs[index].style.display = "flex";
         setTimeout(() => {
             att_divs[index].classList.add("active");
         }, 100);
-        curratt=index;
+        btns[curratt].removeAttribute("id");
+        btns[index].setAttribute("id", "btnselect");
+        curratt = index;
     });
 });
