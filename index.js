c26c07a26e43c4cc43be72a0fba0055b4e178113
Rahul <venkatrahulxyz@gmail.com>
2025-03-16 19:06:55 +0530

    corrected shit

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ import questions from "./models/question.js";
 env.config();
 const app = express();
 app.set("view engine", "ejs");
-app.use(express.static("public  "));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
 app.use(
