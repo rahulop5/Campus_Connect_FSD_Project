d78a3627c4df1010b6421a0599a01f703c4e36f7
AtomicX1611 <146544040+AtomicX1611@users.noreply.github.com>
2025-10-09 10:15:34 +0530

    add profile,attendace,bellgraph rendering

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -1,11 +1,14 @@
 import express from "express";
 import {
     studentAttendance,
+    studentAttendancePartial,
     studentDashboard,
     studentDashboardPartial,
     studentGradebyId,
     studentGrades,
+    studentGradesPartial,
     studentProfile,
+    studentProfilePartial,
     updateStudentProfile
 } from "../controllers/studentController.js";
 import Course from "../models/Course.js";
@@ -13,13 +16,17 @@ import Course from "../models/Course.js";
 
 const router = express.Router();
 
-// router.get("/dashboard", studentDashboard);
+router.get("/dashboard", studentDashboard);
 router.get("/profile", studentProfile);
 router.get("/attendance", studentAttendance);
 router.get("/bellgraph", studentGrades);
+
 router.get("/bellgraph-data/:courseId", studentGradebyId);
 router.post('/update', updateStudentProfile);
 
-router.get("/dashboard",studentDashboardPartial)
+router.get("/dashboard/partial",studentDashboardPartial)
+router.get("/attendance/partial", studentAttendancePartial);
+router.get("/profile/partial", studentProfilePartial);
+router.get("/bellgraph/partial", studentGradesPartial);
 
 export default router;
